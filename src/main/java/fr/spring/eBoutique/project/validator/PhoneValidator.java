775453f4b28491cb0ced5546e62f99bb2540c441
Marcelin Dahesh <mdehesh@gmail.com>
2021-03-15 18:40:44 +0100

    validator methods

---
+++ src/main/java/fr/spring/eBoutique/project/validator/PhoneValidator.java
@@ -0,0 +1,26 @@
+package fr.spring.eBoutique.project.validator;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PhoneValidator implements ConstraintValidator<Phone,String> {
+    @Override
+    public void initialize(Phone constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String phonenumber, ConstraintValidatorContext context) {
+
+        if(phonenumber==null){
+            return false;
+        }
+
+        if(phonenumber.matches("01[5-9]\\d{8}$")){
+            return true;
+        }
+
+        return false;
+    }
+}
